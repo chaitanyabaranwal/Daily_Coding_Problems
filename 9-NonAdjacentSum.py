4eec190bc9528810fa03358545d34c71f5141439
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2019-05-19 17:12:39 +0800

    Fix NonAdjacentSum

--- 9-NonAdjacentSum.py
+++ 9-NonAdjacentSum.py
@@ -4,17 +4,15 @@ def recursive_solution(arr):
     return max(arr[0] + recursive_solution(arr[2:]), recursive_solution(arr[1:]))
 
 def iterative_solution(arr):
-    index = 0
-    answer = 0
-    while (index + 1) < len(arr):
-        if arr[index] >= arr[index+1]:
-            answer += arr[index]
-            index += 2
-        else:
-            answer += arr[index+1]
-            index += 3
-    return answer
-        
+    if not arr or arr == []:
+        return 0
+    elif len(arr) <= 2:
+        return max(arr)
+    result = [arr[0], arr[1], arr[2] + arr[0]]
+    for i in range(3, len(arr)):
+        result.append(arr[i] + max(result[i-2], result[i-3]))
+    return max(result[-1], result[-2])
+
 
 assert recursive_solution([2, 4, 6, 2, 5]) == 13
 assert recursive_solution([5, 1, 1, 5]) == 10
